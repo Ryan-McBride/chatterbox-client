2988682fd1afb32606bd88527a2571cbcc1bca5d
Rayn <mcbrideryan89@gmail.com>
2015-07-13 18:14:19 -0700

    slight recaftor to pass some tests

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -14,11 +14,11 @@ $(document).ready(function(){
 
 	$('#refresh').on('click', function(event){ //when the #refresh is clicked, this function is called
 		viewMessages(); //function displayes new messages
-	})
+	});
 
 	$('.username').on('click', 'a' ,function(event){
 		console.log(this.text);
-	})
+	});
 });
 
 var message = function (){
@@ -35,43 +35,46 @@ var message = function (){
 	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
 	    console.error('chatterbox: Failed to send message');
 	  }
-	});}
+	});
+};
+
+var app = {
+  init : function(){
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/chatterbox',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+
+        for (var i = 0; i < data.results.length; i++) {
+          var id = data.results[i].objectId; //stores the users' id in a variable
+
+          if($('#'+id)[0] === undefined && data.results[i].username !== undefined && data.results[i].text !== undefined ){
+            //this if statement skips over tweets that are already populated or empty username or messages
+
+            var $node = ('<div id='+id+' class="chat"></div>'); //makes a jquery node with the id equal to the users id
+            $('#main').prepend($node); //appends the node to the DOM's body
+            $('#'+id).append('<p class = username></p>'); //adds a p-tag for the user name
+            $('#'+id).find('.username').text(data.results[i].username); //inserting clean(ie less system hackable) username text
+
+            $('#'+id).append('<p class = message></p>'); //adds a p-tag for the message
+            $('#'+id).find('.message').text(data.results[i].text);//inserting clean(ie less system hackable) username message
+
+            $('#'+id).append('<p class = roomname></p>'); //adds a p-tag for the message
+            $('#'+id).find('.roomname').text('room name: ' + data.results[i].roomname);//inserting clean(ie less system hackable) username message
+
+           }
+        }
+
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message');
+      } 
+     }); 
+  }
 
 
-var viewMessages = function (parameter){
-	$.ajax({
-	  url: 'https://api.parse.com/1/classes/chatterbox',
-	  type: 'GET',
-	  contentType: 'application/json',
-	  success: function (data) {
-
-	    for (var i = 0; i < data.results.length; i++) {
-	    	var id = data.results[i].objectId; //stores the users' id in a variable
-
-	  		if($('#'+id)[0] === undefined && data.results[i].username !== undefined && data.results[i].text !== undefined ){
-	  			//this if statement skips over tweets that are already populated or empty username or messages
-
-		    	var $node = ('<div id='+id+' class="chat"></div>'); //makes a jquery node with the id equal to the users id
-		    	$('#main').prepend($node); //appends the node to the DOM's body
-		    	$('#'+id).append('<p class = username></p>'); //adds a p-tag for the user name
-		    	$('#'+id).find('.username').text(data.results[i].username); //inserting clean(ie less system hackable) username text
-
-		    	$('#'+id).append('<p class = message></p>'); //adds a p-tag for the message
-		    	$('#'+id).find('.message').text(data.results[i].text);//inserting clean(ie less system hackable) username message
-
-		    	$('#'+id).append('<p class = roomname></p>'); //adds a p-tag for the message
-		    	$('#'+id).find('.roomname').text('room name: ' + data.results[i].roomname);//inserting clean(ie less system hackable) username message
-
-			}
-	    };
-
-	  },
-	  error: function (data) {
-	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-	    console.error('chatterbox: Failed to send message');
-	  }	
-});}
-
-
+};
 viewMessages();
 
